b6674df75eb6d3d8ad5b6a6487d5b42929d4c913
Trung Thanh Nguyen <trungnguyenthanh.eworking@gmail.com>
2026-06-10 12:25:44 +0700

    Fix file paths for Streamlit pages

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 dashboard = st.Page(
-    "pages/tmp_strl_app.py",
+    "tmp_strl_app.py",
     title="Demo",
     default=True
 )
 
 settings = st.Page(
-    "pages/eval_strl_app.py",
+    "eval_strl_app.py",
     title="Đánh giá"
 )
 
